51d792ddc1083e24714752d0db89ad3a2572dfc8
SUPERXij <86589033+SUPERXij@users.noreply.github.com>
2021-07-20 15:19:15 +0800

    Add files via upload

--- 4_舰长.py
+++ 4_舰长.py
@@ -4,24 +4,53 @@ import csv
 import itertools
 from tqdm import tqdm
 
-result1 = []
-for i in tqdm(range(0, 400)):
-    url = f"https://api.live.bilibili.com/xlive/app-room/v2/guardTab/" \
-          f"topList?roomid=22632424&page={i}&ruid=672353429&page_size=29"
+
+def captain_list(uid, roomid):
+    # 新建一个name_temp的空列表,用于暂时储存每次遍历的name数据
+    name_temp = []
+    uid = str(uid)
+    roomid = str(roomid)
+    # 访问舰长数据页，获取page数和舰长总数
+    url = "https://api.live.bilibili.com/xlive/app-room/v2/guardTab/topList?" \
+          "roomid=" + roomid + "&page=1&ruid=" + uid + "&page_size=29"
     resp = requests.get(url)
-    name_data = re.compile(r'"uid":(?P<uid>.*?),".*?"username":"(?P<name>.*?)",', re.S)
-    name = name_data.finditer(resp.text)
-    result = itertools.chain(result1, name)
-    result1 = result
+    page_data = re.compile(r'{"num":(?P<num>.*?),"page":(?P<page>.*?),"', re.S)
+    captain_num = page_data.search(resp.text).group("num")
+    page_num = page_data.search(resp.text).group("page")
+    print("舰长总数为：%s" % captain_num)
+    # print(page_num)
+    page_num = int(page_num)
+
     resp.close()
+    # range里面的数据看舰长数情况定
+    for i in tqdm(range(page_num+1)):
+        # 根据uid查询舰长数据
+        url = f"https://api.live.bilibili.com/xlive/app-room/" \
+              f"v2/guardTab/topList?roomid="+roomid+"&" \
+              f"page={i}&ruid="+uid+"&page_size=29"
+        resp = requests.get(url)
+        # 获取resp里面的舰长uid和name数据
+        name_data = re.compile(r'"uid":(?P<uid>.*?),".*?,"rank":(?P<rank>.*?),"username":"(?P<name>.*?)",', re.S)
+        name_page = name_data.finditer(resp.text)
+        # 将每次遍历的name存储
+        name = itertools.chain(name_temp, name_page)
+        name_temp = name
+        resp.close()
 
-fp = open("databili.csv", mode="w", encoding="utf8", newline="")
-csvwriter = csv.writer(fp)
+    # 将数据存放在databilibili中
+    fp = open("databili.csv", mode="w", encoding="utf8", newline="")
+    csvwriter = csv.writer(fp)
 
-for it in result:
-    # print(it.group("name"))
-    dic = it.groupdict()
-    csvwriter.writerow(dic.values())
+    for it in name:
+        # print(it.group("name"))
+        dic = it.groupdict()
+        csvwriter.writerow(dic.values())
 
-fp.close()
-print("over!")
+    fp.close()
+    print("over!")
+
+# 嘉然 uid：672328094，roomid：22637261
+# 贝拉 uid：672353429，roomid：22632424
+
+
+captain_list(672328094, 22637261)
--- 4_舰长2.0.py
+++ 4_舰长2.0.py
@@ -1,23 +1,25 @@
 import requests
 import re
 import itertools
+import time
 from tqdm import tqdm
 
 
-result1 = []
+uid_temp = []
+n = 0
 level1, level2, level3, level4, level5, level6 = 0, 0, 0, 0, 0, 0
 
-for i in tqdm(range(1, 5)):
+for i in tqdm(range(1, 400)):
     url = f"https://api.live.bilibili.com/xlive/app-room/v2/guardTab/" \
           f"topList?roomid=22632424&page={i}&ruid=672353429&page_size=29"
     resp = requests.get(url)
     uid_data = re.compile(r'"uid":(?P<uid>.*?),"', re.S)
-    uid = uid_data.finditer(resp.text)
-    result = itertools.chain(result1, uid)
-    result1 = result
+    uid_page = uid_data.finditer(resp.text)
+    uid = itertools.chain(uid_temp, uid_page)
+    uid_temp = uid
     resp.close()
 
-for it in result:
+for it in tqdm(uid):
     # print(it.group("uid"))
     uid_number = str(it.group("uid"))
     url = "https://api.bilibili.com/x/space/acc/info?mid="+uid_number+"&jsonp=jsonp"
@@ -25,8 +27,9 @@ for it in result:
     level_data = re.compile(r'"level":(?P<level>.*?),"', re.S)
     level = level_data.search(resp.text).group("level")
     resp.close()
+    time.sleep(0.5)
     # print(level)
-
+    n += 1
     level_number = int(level)
     if level_number == 1:
         level1 += 1
@@ -47,5 +50,6 @@ print("3级用户:%d" % level3)
 print("4级用户:%d" % level4)
 print("5级用户:%d" % level5)
 print("6级用户:%d" % level6)
+print("统计%d个用户" % n)
 
 
